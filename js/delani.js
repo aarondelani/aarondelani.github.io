f80842f0f6015b9cb501908bc2947d0569a8b60c
Aaron Delani <today0@gmail.com>
2016-12-28 17:15:27 -0800

    Source formatting and adding styles.

--- js/delani.js
+++ js/delani.js
@@ -1,4 +1,57 @@
 (function delani(i) {
+body = document.body;
+
+// Creating DOM Objects on the Fly
+createObj = function (o, attrs) {
+	// Parameters example:
+	// o = {
+	// 	classes: "list-group-item block-item",
+	// 	tag: 'li',
+	// 	contents: 'string',
+	// 	id: 'id'
+	// };
+
+	if (o.tag) {
+		var obj = document.createElement(o.tag);
+
+		if (o.classes) {
+			obj.className = o.classes;
+		} false;
+		if (o.contents) {
+			// Accepts strings and renders HTML
+			obj.innerHTML = o.contents;
+		} false;
+		if (o.id) {
+			obj.id = o.id;
+		} false;
+	} else {
+		// If no tags exist, just fill with content
+		var obj = document.createElement('div');
+
+		obj.innerHTML = o;
+		obj = obj.firstChild;
+	}
+
+	// attrs is an array of objects with key value pairing
+	// attr = [
+	// {"i":"a"},
+	// {key:value}
+	// ];
+	if (attrs) {
+		for (var i = 0, l = attrs.length; i < l; i++) {
+			var key = "",
+			val = Object.values(attrs[i]);
+
+			for (key in attrs[i]) {
+				key = key;
+			}
+
+			obj.setAttribute(key, val[0]);
+		}
+	}
+
+	return obj;
+};
 
 // Parses array of email objects
 emails = function (arr) {
@@ -12,6 +65,14 @@ emails = function (arr) {
 			emails[i].href = str;
 		}
 	}
+}
+
+createPoster = function (params) {
+	var params = params;
+	console.log(params)
+
+		var poster = createObj('<div class="poster"/>', params);
+		body.appendChild(poster);
 }
 
 })();
